13fe576ee4803187c1198be67037a74046f512d2
Joel Sebastian Benites Malasquez <169000532+skilinder753@users.noreply.github.com>
2024-07-18 03:11:52 +0000

    Configuración

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install sl
-export PATH=$PATH:/usr/games
+# Detiene el script si ocurre un error
+set -e
+
+# Verifica si el script se ejecuta como superusuario
+if [ "$(id -u)" != "0" ]; then
+   echo "Este script debe ser ejecutado como root" 1>&2
+   exit 1
+fi
+
+# Actualiza e instala en un solo paso
+sudo apt-get update && sudo apt-get install -y sl
+
+# Exporta la ruta para incluir /usr/games, si no está ya incluida
+if [[ ":$PATH:" != *":/usr/games:"* ]]; then
+    export PATH=$PATH:/usr/games
+fi
